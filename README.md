772e233195784922660c01b029dfe5763c7ed578
peterbanfi <p6t6rbanfi@gmail.com>
2020-11-14 14:29:28 +0100

    feat[general]: add v1.0.0.0

---
+++ README.md
@@ -0,0 +1,26 @@
+# Copy Code README
+
+Copy code with template literals for easy paste to editors' whats supports markdown.
+
+## Features
+
+_Transfrom this:_
+const a;
+
+_to this:_
+```
+const a;
+```
+
+_with adding template literals and new lines:_
+\`\`\`\n${copiedContent}\n\`\`\`
+
+## Requirements
+
+-
+
+## Extension Settings
+
+-
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,33 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	// The command has been defined in the package.json file
+	const disposable = vscode.commands.registerCommand('copy-code.copycode', () => {
+
+		// do a regular copy then edit it
+		vscode.commands.executeCommand('editor.action.clipboardCopyAction').then(async () => {
+			// get copied content
+			const clipboardContent = await vscode.env.clipboard.readText();
+
+			// add edited content to clipboard 
+			vscode.env.clipboard.writeText(`\`\`\`\n${clipboardContent}\n\`\`\``);
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
