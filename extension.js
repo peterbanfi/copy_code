b71d46996fd63a445916b343993a2ce9c072cee4
peterbanfi <p6t6rbanfi@gmail.com>
2020-11-16 13:07:54 +0100

    feat[extension]: syntax highlight

--- extension.js
+++ extension.js
@@ -9,13 +9,16 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	const disposable = vscode.commands.registerCommand('copy-code.copycode', () => {
 
+		const fileName = vscode.window.activeTextEditor.document.fileName
+		const fileExtension = fileName.split('.')[fileName.split('.').length - 1];
+
 		// do a regular copy then edit it
 		vscode.commands.executeCommand('editor.action.clipboardCopyAction').then(async () => {
 			// get copied content
 			const clipboardContent = await vscode.env.clipboard.readText();
 
 			// add edited content to clipboard 
-			vscode.env.clipboard.writeText(`\`\`\`\n${clipboardContent}\n\`\`\``);
+			vscode.env.clipboard.writeText(`\`\`\` ${fileExtension}\n${clipboardContent}\n\`\`\``);
 		});
 	});
 
